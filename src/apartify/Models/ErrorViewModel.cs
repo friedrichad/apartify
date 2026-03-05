d27695d43989cef2e26c3a7b94876c06c7c85c5f
friedrich <wquangu@gmail.com>
2026-03-05 21:46:10 +0700

    Refactor project by using wpf framework

--- src/apartify/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace apartify.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
