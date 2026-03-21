bcc0b6f732ef93cc65fe35e4a0b3c638edea8cec
friedrich <wquangu@gmail.com>
2026-03-21 15:57:34 +0700

    Move MainWindow to Views Folder

---
+++ src/Apartify/Views/ApartmentWindow.xaml.cs
@@ -0,0 +1,14 @@
+using Apartify.Models;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace Apartify.Views
+{
+    public partial class ApartmentWindow : Window
+    {
+        public ApartmentWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
