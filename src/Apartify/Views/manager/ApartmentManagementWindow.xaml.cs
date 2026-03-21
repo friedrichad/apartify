76c7d08edec118e95a625e05b6dc61881780851f
friedrich <wquangu@gmail.com>
2026-03-21 15:39:08 +0700

    initialize manager branch & window

---
+++ src/Apartify/Views/manager/ApartmentManagementWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Apartify.Views.manager
+{
+    /// <summary>
+    /// Interaction logic for ApartmentManagementWindow.xaml
+    /// </summary>
+    public partial class ApartmentManagementWindow : Window
+    {
+        public ApartmentManagementWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
